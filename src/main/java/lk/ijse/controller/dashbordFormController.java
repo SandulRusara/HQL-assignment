f097239276d24192ca8612c6942e5a2130f29c68
SandulRusara <sandulrusara8@gmail.com>
2024-02-15 21:51:37 +0530

    update

--- src/main/java/lk/ijse/controller/dashbordFormController.java
+++ src/main/java/lk/ijse/controller/dashbordFormController.java
@@ -3,7 +3,9 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXTextArea;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.text.Font;
 import lk.ijse.bo.AuthorBoImpl;
 import lk.ijse.bo.BookBoImpl;
 import lk.ijse.confi.FactoryConfiguration;
@@ -11,10 +13,12 @@ import lk.ijse.dto.BookDTO;
 import lk.ijse.entity.Author;
 import org.hibernate.Session;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class dashbordFormController {
+public class dashbordFormController implements Initializable {
 
     @FXML
     private JFXTextArea txtArea;
@@ -54,7 +58,7 @@ public class dashbordFormController {
         for (int i = 0; i <arrayList.size(); i++) {
             total+=arrayList.get(i);
         }
-        txtArea.appendText("Average"+(total/x));
+        txtArea.appendText("Average "+(total/x));
     }
 
     @FXML
@@ -81,4 +85,8 @@ public class dashbordFormController {
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        txtArea.setFont(Font.font(15));
+    }
 }
